5946ecd945418de673d89adf99a89a363d714c50
Lucas Stoller <l.s.stoller@gmail.com>
2023-08-07 21:24:31 -0300

    Adding ex1

---
+++ ex1.py
@@ -0,0 +1,11 @@
+"""
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing!")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+"""
+name = "Lucas"
+print(f"My name is {name}")
